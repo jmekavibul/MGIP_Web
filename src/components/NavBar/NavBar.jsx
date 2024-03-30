bcf244bd5a0255932b1a3ba65d9a75d31d43dbc9
Shawn Lai <plai@kadsci.com>
2024-03-29 23:05:27 -0400

    made initial navbar changes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,15 +3,32 @@ import { Link } from 'react-router-dom';
 import './NavBar.css'
 export const NavBar = () => {
   return (
-    <div className='nav'>
-        <div>
-            <img src="mgip-logo.png" width={200}></img>
+    // <div className='nav'>
+    //     <div>
+    //         <img src="mgip-logo.png" width={200}></img>
+    //     </div>
+    //     <ul className='nav-menu'>
+    //         <li><Link path="/">About</Link></li>
+    //         <li><Link path="/team">Team</Link></li>
+    //         <li className='nav-contact'><Link path="/contact">Contact</Link></li>
+    //     </ul>
+    // </div>
+    <nav className="navbar">
+        <div className="navbar-logo">
+        <img src="mgip-logo.png" alt="Logo" />
         </div>
-        <ul className='nav-menu'>
+        <ul className="navbar-links">
             <li><Link path="/">About</Link></li>
             <li><Link path="/team">Team</Link></li>
             <li className='nav-contact'><Link path="/contact">Contact</Link></li>
         </ul>
-    </div>
+        {/* <div className="navbar-login">
+        <a href="#login">Client Login</a>
+        </div>
+        <div className="navbar-search">
+        <input type="search" placeholder="Search" />
+        <button type="submit">🔍</button>
+        </div> */}
+    </nav>
   )
 }
