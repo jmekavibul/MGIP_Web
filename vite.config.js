6d41d3c896f6b6f8121801f0adb8b6c76547e9db
Shawn Lai <plai@kadsci.com>
2024-04-02 00:48:39 -0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-    // ... any other build options you want to specify
   },
 })
