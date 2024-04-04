87d6ab7cc4ec76485331673fe3dcdac0c292fe2c
Shawn Lai <shawnlai@Shawns-MacBook-Pro.local>
2024-04-04 15:36:51 -0400

    changed teams related stuff

--- App.jsx
+++ App.jsx
@@ -6,18 +6,58 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import './App.css'
 import joe from './pictures/Joe-Muncy.jpg';
+import mark from './pictures/Mark.jpg';
+import martin from './pictures/Martin.jpg';
+import scott from './pictures/scott.jpg';
+import john from './pictures/John.jpg';
+
 function App() {
     const teamMembers = [
         {
           id: 1,
-          firstName: 'Joe',
-          lastName: 'Ken',
-          title: 'Lawyer',
+          name: 'Joe M. (Ken) Muncy',
+          title: 'PRINCIPAL SHAREHOLDER',
           location: 'Alexandria Virginia',
           phone: '111-111-1111',
           photo: joe, // Replace with the path to the photo
           keywords: 'tech specialist'
         },
+        {
+            id: 2,
+            name: 'Martin R. Geissler',
+            title: 'PRINCIPAL SHAREHOLDER',
+            location: 'Alexandria Virginia',
+            phone: '111-111-1111',
+            photo: martin, // Replace with the path to the photo
+            keywords: 'tech specialist'
+          },
+          {
+            id: 3,
+            name: 'Mark E. Olds',
+            title: 'PRINCIPAL SHAREHOLDER',
+            location: 'Alexandria Virginia',
+            phone: '111-111-1111',
+            photo: mark, // Replace with the path to the photo
+            keywords: 'tech specialist'
+          },
+          {
+            id: 4,
+            name: 'Scott L. Lowe',
+            title: 'PRINCIPAL SHAREHOLDER',
+            location: 'Alexandria Virginia',
+            phone: '111-111-1111',
+            photo: scott, // Replace with the path to the photo
+            keywords: 'tech specialist'
+          },
+          {
+            id: 5,
+            name: 'John L. Ciccozzi',
+            title: 'PRINCIPAL SHAREHOLDER',
+            location: 'Alexandria Virginia',
+            phone: '111-111-1111',
+            photo: john, // Replace with the path to the photo
+            keywords: 'tech specialist'
+          },
         // ...more members
       ];
     return(
--- components/Team/Team.jsx
+++ components/Team/Team.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './Team.css';
-import joe from '../../pictures/Joe-Muncy.jpg';
+import teamphoto from '../../pictures/AITeamphoto.jpg'
 
 export const Team = ({ members }) => {
     const [searchQuery, setSearchQuery] = useState('');
@@ -25,53 +25,56 @@ export const Team = ({ members }) => {
   };
 
   const filteredMembers = members.filter((member) =>
-    member.lastName.toLowerCase().startsWith(searchQuery.toLowerCase())
+    member.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
     <div className="team-container">
-    <div className="search-container">
-        <div className='search-items'>
+      <div className='background' style={{ backgroundImage: `url(${teamphoto})` }}/>
+      <div className="search-container">
+          <div className='search-items'>
+              <div className="search-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
+              </div>
 
-            <div className="search-icon">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
-            </div>
+              <input
+              className="the-search-bar"
+              type="text"
+              placeholder="Search by Last Name"
+              value={searchQuery}
+              onChange={handleSearchChange}
+              />
+          </div>
+          
 
-            <input
-            className="the-search-bar"
-            type="text"
-            placeholder="Search by Last Name"
-            value={searchQuery}
-            onChange={handleSearchChange}
-            />
-        </div>
-        
-
-        <div className="alphabet">
-        {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((letter) => (
-            <a
-            key={letter}
-            href={`#${letter}`}
-            className={activeLetter === letter ? 'active' : ''}
-            onClick={(e) => {
-                e.preventDefault();
-                handleLetterClick(letter);
-            }}
-            >
-            {letter}
-            </a>
-        ))}
-        </div>
-    </div>
+          <div className="alphabet">
+          {'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('').map((letter) => (
+              <a
+              key={letter}
+              href={`#${letter}`}
+              className={activeLetter === letter ? 'active' : ''}
+              onClick={(e) => {
+                  e.preventDefault();
+                  handleLetterClick(letter);
+              }}
+              >
+              {letter}
+              </a>
+          ))}
+          </div>
+      </div>
       <div className="team-grid">
         {filteredMembers.map((member, index) => (
           <div className="team-member" key={index}>
-            <img src={member.photo} alt={member.name} /> {/* Ensure correct image path */}
+            <img src={member.photo} alt={member.name} />
             <div className="member-info">
-              <h3>{`${member.firstName} ${member.lastName}`}</h3>
-              <p>{member.title}</p>
-              <p>{member.location}</p>
-              <p>{member.phone}</p>
+              <h3>{member.name}</h3>
+              <div className="additional-info">
+
+                <p>{member.title}</p>
+                <p>{member.location}</p>
+                <p>{member.phone}</p>
+              </div>
             </div>
           </div>
         ))}
