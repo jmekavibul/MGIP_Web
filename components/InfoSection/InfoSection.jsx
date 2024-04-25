631dbc0be0a92b975175b3608eb82f11ae38fe11
Shawn Lai <plai@kadsci.com>
2024-04-24 23:44:47 -0400

    changes

--- components/InfoSection/InfoSection.jsx
+++ components/InfoSection/InfoSection.jsx
@@ -49,25 +49,25 @@ const InfoSection = () => {
 
     return (
         <div className="info-section">
-            <div ref={sectionRef} className={`services-section ${isVisible ? 'visible' : ''}`}>
+            <div className='service-section'>
                 <div className="services-header">
                     <h2>Firm Highlight</h2>
                     <a href="/MGIP_Web/services" className="all-services">See all services</a>
                 </div>
                 <Awards></Awards>
-                <div className="service-points">
+                <div ref={sectionRef} className={`service-points ${isVisible ? 'visible' : ''}`}>
                 <div className="content-box-wrapper">
                         <ContentBox content={<div className="service-point">
                             <h1>Patent</h1>
                             <p>Protect industrial innovations - secure your innovations and creativity for the long term with patents</p>
-                        </div>} onClick={handleOpenPopup('Detail about Patent Service')}/>
+                        </div>} onClick={handleOpenPopup('We provide legal services to large domestic and foreign corporations and to a wide range of mid-sized and start-up organizations. Our technical, business, and legal experience allows us to assist our clients in obtaining protection for their intellectual property in an efficient and cost-effective manner.')}/>
                     </div>
                 <div className="content-box-wrapper">
                         <ContentBox content={<div className="service-point">
                             <h1>Trademark</h1>
                             <p>A strong trade mark – as reliable as your good name
 Trade mark protection as a guarantee for your success on the market.</p>
-                        </div>} onClick={handleOpenPopup('Detail about Trade Mark Service')}/>
+                        </div>} onClick={handleOpenPopup('We provide legal services to large domestic and foreign corporations and to a wide range of mid-sized and start-up organizations. Our technical, business, and legal experience allows us to assist our clients in obtaining protection for their intellectual property in an efficient and cost-effective manner.')}/>
                     </div>
 
 
@@ -75,7 +75,7 @@ Trade mark protection as a guarantee for your success on the market.</p>
                         <ContentBox content={<div className="service-point">
                             <h1>Copyright</h1>
                             <p>Protect unique designs in international competitions, secure intellectual property rights</p>
-                        </div>} onClick={handleOpenPopup('Detail about Design Service')}/>
+                        </div>} onClick={handleOpenPopup('We provide legal services to large domestic and foreign corporations and to a wide range of mid-sized and start-up organizations. Our technical, business, and legal experience allows us to assist our clients in obtaining protection for their intellectual property in an efficient and cost-effective manner.')}/>
                     </div>
                     {/* Add more service points as needed */}
                 </div>
