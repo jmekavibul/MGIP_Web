c691cd5b9c67247d7c2b05566a69c23263af0337
Shawn Lai <plai@kadsci.com>
2024-06-24 05:40:29 -0400

    changeS

---
+++ components/FlowbiteFooter/FlowbiteFooter.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+
+export default function FlowbiteFooter() {
+  return (
+    <MDBFooter bgColor='light' className='text-center text-lg-start text-muted'>
+      <section className='d-flex justify-content-center justify-content-lg-between p-4 border-bottom'>
+        <div className='me-5 d-none d-lg-block'>
+          <span>Get connected with us on social networks:</span>
+        </div>
+
+      </section>
+
+      <section className=''>
+        <MDBContainer className='text-center text-md-start mt-5'>
+          <MDBRow className='mt-3'>
+            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>
+              </h6>
+              <p>
+                Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+                consectetur adipisicing elit.
+              </p>
+            </MDBCol>
+
+            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
+              <h6 className='text-uppercase fw-bold mb-4'>Products</h6>
+            </MDBCol>
+
+
+
+          </MDBRow>
+        </MDBContainer>
+      </section>
+
+      <div className='text-center p-4' style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
+        © 2021 Copyright:
+        <a className='text-reset fw-bold' href='https://mdbootstrap.com/'>
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
+  );
+}
--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,20 +1,49 @@
 import React from 'react';
-import './Footer.css'; // Make sure to create and link this CSS file
+import { Link } from 'react-router-dom';
 
-const Footer = () => {
+import './Footer.css'; // Ensure CSS is linked
+import { FaLinkedin } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import mgipLogo from '../../pictures/mgip-logo.png';
+
+const Footer = ({credits}) => {
+  const currentYear = new Date().getFullYear(); // Gets the current year
+
   return (
-    <footer className="site-footer">
-      <div className="footer-content">
-        <div className="footer-links">
-          <a href="/disclaimer">Disclaimer</a>
-        </div>
-
-        <div className="footer-legal">
-          <p>© MUNCY, GEISSLER OLDS & LOWE 2024 All Rights Reserved.</p>
-        </div>
+    <>
+      <div style={{ width: '100%', height: '20px', backgroundColor: "#6C8EAD" }} />
+      <div className='footer-container'>
+        <footer className="footer">
+          <div className="footer-content">
+            <div className="footer-section company-info">
+              <img src={mgipLogo} alt="mgip Logo" style={{ maxWidth: '250px', height: 'auto' }} />
+            </div>
+            <div className='link-parent'>
+              <div className="footer-section quick-links">
+                <h4 style={{ marginBottom: '0px' }}>QUICK LINKS</h4>
+                <ul style={{ marginTop: '10px' }}>
+                  <li><Link to="/" >About</Link></li>
+                  <li><Link to="/team">Team</Link></li>
+                </ul>
+              </div>
+              <div className="footer-section contact-info">
+                <h4 style={{ marginBottom: '0px' }}>GET IN TOUCH</h4>
+                <ul style={{ marginTop: '10px' }}>
+                  <li><FaLinkedin /> <a href="https://www.linkedin.com">LinkedIn</a></li>
+                  <li><IoMdMail /> <a href="mailto:plai2@gmu.edu">Contact Us</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="footer-credits">
+            <span>© {currentYear} MUNCY, GEISSLER OLDS & LOWE All Rights Reserved.</span>
+            {credits && (<span><a href="https://www.flaticon.com/free-icons/project" style={{ color: 'white', textDecoration: 'none' }} title="project icons">Project icons created by surang - Flaticon</a></span>)}
+            <span>Website by Minty Web Designs</span>
+          </div>
+        </footer>
       </div>
-    </footer>
+    </>
   );
-};
+}
 
 export default Footer;
