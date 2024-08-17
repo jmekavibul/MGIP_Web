f39a5c20a13c3eba835319862f6fe7a13b3f98ad
Shawn Lai <plai@kadsci.com>
2024-08-16 21:15:08 -0400

    contact us box

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -1,15 +1,13 @@
-import GoogleMapComp from "../GoogleMap.jsx/GoogleMapComp";
 import ContactForm from "./ContactForm";
 import './Contact.css';  // Ensure this CSS file has the required styles
 
 export default function Contact() {
     return (
-        <div className="contact-container">
-            <div className="form-section">
-                <ContactForm />
+        <div className="contact-page-container">
+            <div className="contact-banner">
             </div>
-            <div className="map-section">
-                <GoogleMapComp />
+            <div className="contact-form-section">
+                <ContactForm />
             </div>
         </div>
     );
