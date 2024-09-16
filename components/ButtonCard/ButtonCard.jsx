08dd6e64c0c2f0c00c65dffdce7fb9f66a6e3ca3
Shawn Lai <plai@kadsci.com>
2024-09-16 14:10:10 -0400

    some chages

--- components/ButtonCard/ButtonCard.jsx
+++ components/ButtonCard/ButtonCard.jsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from './ButtonCard.module.css';
 
 const ButtonCard = ({ backgroundColor, title, content, buttonText, borderRadius, textColor, url, center }) => {
+  const cardRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add(styles['fade-in']);
+          observer.disconnect(); // Stop observing once the animation is triggered
+        }
+      },
+      { threshold: 0.2 } // Trigger when 20% of the element is visible
+    );
+
+    if (cardRef.current) {
+      observer.observe(cardRef.current);
+    }
+
+    return () => {
+      if (cardRef.current) {
+        observer.unobserve(cardRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className={styles.buttonCard} style={{ backgroundColor, borderRadius, color: textColor }}>
-      <h1 className={styles.buttonTitle} style={{ color: textColor, textAlign: center ? 'center' : 'left' }}>{title}</h1>
+    <div
+      ref={cardRef}
+      className={styles.buttonCard}
+      style={{ backgroundColor, borderRadius, color: textColor }}
+    >
+      <h1 className={styles.buttonTitle} style={{ color: textColor, textAlign: center ? 'center' : 'left' }}>
+        {title}
+      </h1>
       {Array.isArray(content) ? (
         content.map((text, index) => (
           <p key={index} className={styles.description} style={{ color: textColor, marginBottom: '1em' }}>
@@ -12,10 +42,15 @@ const ButtonCard = ({ backgroundColor, title, content, buttonText, borderRadius,
           </p>
         ))
       ) : (
-        <p className={styles.description} style={{ color: textColor }}>{content}</p>
+        <p className={styles.description} style={{ color: textColor }}>
+          {content}
+        </p>
       )}
       {buttonText && (
-        <button className={styles.cardButton} onClick={() => window.location.href = url}>
+        <button
+          className={styles.cardButton}
+          onClick={() => (window.location.href = url)}
+        >
           {buttonText}
         </button>
       )}
--- components/MyHeroSlider/MyHeroSlider.jsx
+++ components/MyHeroSlider/MyHeroSlider.jsx
@@ -94,7 +94,23 @@ const MyHeroSlider = () => {
         </Slide>
         <Nav />
       </HeroSlider>
-      <button className="scroll-button" onClick={handleScrollDown}></button>
+      <button className="scroll-button" onClick={handleScrollDown}>
+        <svg
+            width="50"
+            height="50"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+            d="M4 8L12 16L20 8"
+            stroke="white"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            />
+        </svg>
+        </button>
     </div>
   );
 };
